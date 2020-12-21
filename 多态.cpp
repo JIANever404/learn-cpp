92992179579fbc5a36834a0f96a0518be4b37e8f
jiajiangze <jiangze.jia@i-tage.com>
2020-12-21 16:48:36 +0800

    多态--虚函数

---
+++ 多态.cpp
@@ -0,0 +1,64 @@
+#include <iostream> 
+using namespace std;
+
+class Shape {
+   protected:
+      int width, height;
+   public:
+      Shape( int a=0, int b=0)
+      {
+         width = a;
+         height = b;
+      }
+      //放置关键字virtual
+      //虚函数
+      //虚函数 是在基类中使用关键字 virtual 声明的函数。
+      //在派生类中重新定义基类中定义的虚函数时，会告诉编译器不要静态链接到该函数。
+      //我们想要的是在程序中任意点可以根据所调用的对象类型来选择调用的函数，这种操作被称为动态链接，或后期绑定。
+      virtual int area()
+      {
+         cout << "Parent class area :" <<endl;
+         return 0;
+      }
+};
+class Rectangle: public Shape{
+   public:
+      Rectangle( int a=0, int b=0):Shape(a, b) { }
+      int area ()
+      { 
+         cout << "Rectangle class area :" <<endl;
+         return (width * height); 
+      }
+};
+class Triangle: public Shape{
+   public:
+      Triangle( int a=0, int b=0):Shape(a, b) { }
+      int area ()
+      { 
+         cout << "Triangle class area :" <<endl;
+         return (width * height / 2); 
+      }
+};
+// 程序的主函数
+int main( )
+{
+   Shape *shape;
+   Rectangle rec(10,7);
+   Triangle  tri(10,5);
+ 
+   // 存储矩形的地址
+   shape = &rec;
+   // 调用矩形的求面积函数 area
+   shape->area();
+ 
+   // 存储三角形的地址
+   shape = &tri;
+   // 调用三角形的求面积函数 area
+   shape->area();
+   //输出为
+   //Rectangle class area
+   //Triangle class area
+   //每个子类都有一个函数 area() 的独立实现。这就是多态的一般使用方式。
+   //有了多态，您可以有多个不同的类，都带有同一个名称但具有不同实现的函数，函数的参数甚至可以是相同的。
+   return 0;
+}
