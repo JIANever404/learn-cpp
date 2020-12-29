af4bdb3bec9b56ed7e6dcaccd4c0081ba2d79dbd
jiajiangze <jiangze.jia@i-tage.com>
2020-12-29 15:25:07 +0800

    预处理器 信号处理

---
+++ 预定义宏.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 
+    cout << "File = " << __FILE__ << '\n'
+       << "LINE = " << __LINE__ << '\n'
+       << "DATE = " << __DATE__ << '\n'
+       << "TIME = " << __TIME__
+       << endl; 
+    return 0;
+}
